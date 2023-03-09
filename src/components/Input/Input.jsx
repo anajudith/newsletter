29cd7c4a2a33d36a66eef692df2280e90e5ca0e5
Ana Judith <ana-judth@hotmail.com>
2023-03-09 16:57:51 -0300

    implementando formulario

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,13 @@
+const Input = (props) => {
+  return (
+    <input
+      className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none
+      placeholder:text-gray-400 placeholder:dark:text-gray-600 placeholder:text-xs placeholder:uppercase placeholder:font-bold
+      focus: border-2 focus:border-alura-200 focus:dark:border-alura-100 invalid:focus:border-2 focus:invalid:border-red-400
+      invalid:dark:border-2 focus:invalid:dark:border-red-400"
+      {...props}
+    ></input>
+  );
+};
+
+export default Input;
