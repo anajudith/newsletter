36fc9359ef244c2b78b87323be76065ef95f00f0
Ana Judith <ana-judth@hotmail.com>
2023-03-09 17:30:00 -0300

    explorando a estilizacao do tailwind

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,3 @@
-
-
 const Form = ({ onSubmit }) => {
   const safeSubmit = (event) => {
     event.preventDefault();
@@ -13,12 +11,19 @@ const Form = ({ onSubmit }) => {
       onSubmit={safeSubmit}
       className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5"
     >
-      <input type="text" className="alura-input" required placeholder="Insira seu nome..." />
-      <input type="email" className="alura-input" required placeholder="Insira seu email..." />
-      <button
-        type="submit"
-        className="alura-button"
-      >
+      <input
+        type="text"
+        className="alura-input"
+        required
+        placeholder="Insira seu nome..."
+      />
+      <input
+        type="email"
+        className="alura-input"
+        required
+        placeholder="Insira seu email..."
+      />
+      <button type="submit" className="alura-button">
         Seguir
       </button>
     </form>
